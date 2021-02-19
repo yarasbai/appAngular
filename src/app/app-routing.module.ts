867bedde29aac73adf68a5056d870d71d703b798
yarasbai <52915020+yarasbai@users.noreply.github.com>
2021-02-19 20:54:33 +0100

    todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { ProfilComponent } from './profil/profil.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { TodoComponent } from './todo/todo.component';
 
 const routes: Routes = [
 
   {path: "", component:ConnexionComponent},
   {path: "profile", component:ProfilComponent},
   {path: "inscription", component:InscriptionComponent},
+  {path: "todo", component:TodoComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { ProfilComponent } from './profil/profil.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnexionComponent,
     InscriptionComponent,
-    ProfilComponent
+    ProfilComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,

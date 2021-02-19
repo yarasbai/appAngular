a5cfea4e894b4586e0874156515a03634270ab36
yarasbai <52915020+yarasbai@users.noreply.github.com>
2021-02-19 18:52:18 +0100

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ProfilComponent } from './profil/profil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    InscriptionComponent,
+    ProfilComponent
   ],
   imports: [
     BrowserModule,
